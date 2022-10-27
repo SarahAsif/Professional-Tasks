f05bc01b0b4bacf7f88360db1ee91704f3cb4a77
sarahasif <misssarahasif@gmail.com>
2022-10-27 10:42:40 +0500

    Co-authored-by: Hammad Asif <hammad@nitroxis.com>

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import "@fontsource/comfortaa";
-import Login from "./Login";
-import Err from "./Err";
-import Dashboard from "./Dashboard";
-import footer from "./p3.png";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Games from "./components/Games";
+import Chart from "./components/Chart";
+import Rate from "./components/Rate";
+import Last from "./components/Last";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className=" row d-flex justify-content-center align-items-center h-25 m-5 ">
-          <img
-            src={require("./j2.svg").default}
-            alt="mySvgImage"
-            className="svg m-5"
-          />
-        </div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/error" element={<Err />} />
-          </Routes>
-        </BrowserRouter>
-
-        <footer className="text-center text-danger">
-          <p>Version: 1.1.9</p>
-          <img src={footer} />
-        </footer>
-      </div>
+      <Nav />
+      <Main />
+      <Games />
+      <Rate />
+      <Chart />
+      <Last />
+      <Footer />
     </div>
   );
 }
+
 export default App;
